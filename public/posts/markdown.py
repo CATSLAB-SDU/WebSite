54526ecb529df57187cbd2123550befcb317bbc1
yinxiangkai <qingchenke1@gmail.com>
2024-04-08 20:45:30 +0800

    20230408

--- public/posts/markdown.py
+++ public/posts/markdown.py
@@ -3,13 +3,22 @@ import os
 import glob
 from datetime import datetime
 
-def process_math_expressions(md_file_path):
-    # 使用文件名（不含扩展名）作为文章标题
-    title = os.path.splitext(os.path.basename(md_file_path))[0]
-    # 获取当前时间，格式化为ISO 8601格式
-    current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
+def wrap_math_expressions_with_div(content):
+    # 直接将整个$$...$$格式的数学表达式包装在<div>标签中
+    def wrap_with_div(match):
+        # 获取匹配到的整个数学表达式
+        math_expr = match.group(0)
+        # 将整个数学表达式包装在<div>标签中
+        return f'<div>{math_expr}</div>'
     
-    # 构建要添加的元数据字符串
+    # 使用正则表达式匹配整个$$...$$格式的数学表达式，并调用wrap_with_div函数处理
+    wrapped_content = re.sub(r'(\$\$.*?\$\$)', wrap_with_div, content, flags=re.DOTALL)
+    
+    return wrapped_content
+
+def process_math_expressions(md_file_path):
+    title = os.path.splitext(os.path.basename(md_file_path))[0]
+    current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
     metadata = f"""+++
 title = "{title}"
 date = "{current_time}+08:00"
@@ -20,12 +29,10 @@ categories = ["方向"]
 license = '<a rel="license external nofollow noopener noreferrer" href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">CC BY-NC 4.0</a>'
 +++
 """
-    
-    # 读取 Markdown 文件
+
     with open(md_file_path, 'r', encoding='utf-8') as file:
         content = file.read()
-    
-    # 检查是否已经处理过
+
     if content.startswith('+++'):
         print(f"警告：文件 {md_file_path} 似乎已经包含元数据。跳过以避免二次添加。")
         return
@@ -33,33 +40,15 @@ license = '<a rel="license external nofollow noopener noreferrer" href="https://
     # 在文件内容前添加元数据
     content = metadata + content
     
-    # 删除$$...$$中的空行
-    def remove_empty_lines(match):
-        return re.sub(r'\n\s*\n', '\n', match.group(0))
+    # 将$$...$$格式的数学表达式包装在<div>标签中
+    content = wrap_math_expressions_with_div(content)
     
-    content = re.sub(r'(\$\$(.*?)\$\$)', remove_empty_lines, content, flags=re.DOTALL)
-    
-    # 将$$ ... $$格式更改为<div>$$ ... $$</div>
-    content = re.sub(r'(\$\$.*?\$\$)', r'<div>\1</div>', content, flags=re.DOTALL)
-    
-    # 转义$...$格式中的_, *, 和^
-    def escape_characters(match):
-        escaped_content = re.sub(r'(?<!\\)_', r'\_', match.group(0))
-        escaped_content = re.sub(r'(?<!\\)\*', r'\*', escaped_content)
-        escaped_content = re.sub(r'(?<!\\)\^', r'\^', escaped_content)
-        return escaped_content
-    
-    content = re.sub(r'(\$.*?\$)', escape_characters, content, flags=re.DOTALL)
-    
-    # 保存更改
     with open(md_file_path, 'w', encoding='utf-8') as file:
         file.write(content)
     print(f"处理完成：文件 {md_file_path} 已更新。")
 
 def process_all_md_files():
-    # 获取脚本文件所在的目录
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    # 查找目录下的所有.md文件
     md_files = glob.glob(os.path.join(script_directory, '*.md'))
     
     for md_file_path in md_files:
